9ae508791c3266e9264f3bc5065be40ff50bd12f
Birbaumer Nowlan <in17bino@lwb.ch>
2018-03-07 08:48:37 +0100

    Projekt Aick-Off

--- [inf17]_[alpha2]_03_select.sql
+++ [inf17]_[alpha2]_03_select.sql
@@ -5,3 +5,39 @@ Auftrag-Alpha
 von Team Alpha2
 Nowlan Birbaumer und David Lehmann
 */
+
+USE MP3_VERWALTUNG;
+GO
+
+SELECT MAX(Kaufpreis) FROM SONGS;
+
+SELECT * FROM SONGS
+ORDER BY Kaufpreis DESC; 
+--SELECT * FROM SONGS
+
+SELECT MIN(Kaufpreis) FROM SONGS;
+
+SELECT AVG(Kaufpreis) FROM SONGS;
+
+SELECT * FROM SONGS 
+WHERE Interpret = '';
+
+SELECT * FROM SONGS 
+JOIN GENRES ON Genrename=Genre
+WHERE Genrename = '';
+
+SELECT * FROM SONGS 
+JOIN LAENDER ON Herkunftsland=LID
+WHERE Land = '';
+
+SELECT Songtitel, Bewertung FROM SONGS AS s 
+LEFT JOIN (SELECT SID, AVG(Bewertung) AS Bewertung FROM BENUTZERBEWERTUNG
+GROUP BY SID) AS b ON b.SID=s.SID
+ORDER BY Bewertung DESC;
+
+/*Zusatz
+
+
+
+
+*/
